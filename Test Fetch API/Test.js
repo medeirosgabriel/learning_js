00b056fc074e9d739b37e54238b6ba9763c1d692
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2021-04-07 12:25:44 -0300

    test fetch api

---
+++ Test Fetch API/Test.js
@@ -0,0 +1,32 @@
+﻿const fetch = require('node-fetch');
+
+function getUsers() {
+    fetch('https://learningsb.herokuapp.com/users')
+        .then(T => T.json())
+        .then(console.log)
+}
+
+function getUserById(id) {
+    fetch(`https://learningsb.herokuapp.com/users/${id}`)
+        .then(T => T.json())
+        .then(console.log)
+}
+
+function insertUser(user) {
+    
+    let options =  {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    fetch('https://learningsb.herokuapp.com/users', options)
+        .then(res => res.json())
+        .then(json => console.log(json));
+}
+
+let user = {name:"Tadeu Jereira",email:"tadeu@gmail.com",phone:"999922999",password:"12345678"};
+
+//insertUser(user);
+//getUsers();
+getUserById(35);
